e0f88317e328978973ee69862a71ca3fac6a236d
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-11-29 10:59:34 -0800

    add prettier, use it

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 'use strict'
 const alfy = require('alfy')
-const mapping = require('ember-rfc176-data');
+const mapping = require('ember-rfc176-data')
 
 function isDefaultExport(localName) {
-  return !!localName;
+  return !!localName
 }
 
-const pairs = mapping.map(({ global, module, export: exportName, localName, deprecated }) => {
-  if (isDefaultExport(localName)) {
-    return {
-      key: localName,
-      value: `import ${localName} from '${module}'`
-    };
-  }
+const pairs = mapping.map(
+  ({ global, module, export: exportName, localName, deprecated }) => {
+    if (isDefaultExport(localName)) {
+      return {
+        key: localName,
+        value: `import ${localName} from '${module}'`
+      }
+    }
 
-  return {
-    key: exportName,
-    value: `import { ${exportName} } from '${module}'`
+    return {
+      key: exportName,
+      value: `import { ${exportName} } from '${module}'`
+    }
   }
-});
+)
 
 const matches = alfy.inputMatches(pairs, 'key')
 
